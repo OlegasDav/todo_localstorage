6f963fbfb5534edb8d32352be50af8ae33db564e
OlegasDav <84792738+OlegasDav@users.noreply.github.com>
2021-05-26 18:59:09 +0300

    edit_newCard

--- localstorage.js
+++ localstorage.js
@@ -49,13 +49,17 @@ todoForm.addEventListener('submit', e => {
         todoInput.classList.add('is-invalid');
     } else {
         todoInput.classList.remove('is-invalid');
-        var newTodo = `<div class="border border-1 shadow-sm p-3 mb-3  rounded todo-item">
-        <h4 class="mb-3 input-name">${todoInput.value}</h4>
-        <button type="button" class="btn btn-danger delete">Delete</button>
-        <button type="button" class="btn btn-success move-todo">Move to Done</button>
-        <button type="button" class="btn btn-warning edit" data-bs-toggle="modal" data-bs-target="#edit-modal">Edit</button>
-    </div>`;
+        var newTodo = `
+        <div class="border border-1 shadow-sm p-3 mb-3  rounded todo-item">
+            <h4 class="mb-3 input-name">${todoInput.value}</h4>
+            <button type="button" class="btn btn-danger delete">Delete</button>
+            <button type="button" class="btn btn-success move-todo">Move to Done</button>
+            <button type="button" class="btn btn-warning edit" data-bs-toggle="modal" data-bs-target="#edit-modal">Edit</button>
+        </div>`;
         todoList.innerHTML += newTodo;
+        duomenys.push({ todo: todoInput.value, done: true });
+        // paverciam masyva i teksta ir idedam i localstorage
+        localStorage.setItem('Sarasas', JSON.stringify(duomenys));
         todoForm.reset();
     }
 });
